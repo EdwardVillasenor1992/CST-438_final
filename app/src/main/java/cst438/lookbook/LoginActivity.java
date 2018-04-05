802c5f7fc0c1a45ca3335d007fe02e2543742ac4
garc2586 <31326232+garc2586@users.noreply.github.com>
2018-04-05 16:39:41 -0700

    Changed AppCompatActivity to Activity in LoginActivity

--- app/src/main/java/cst438/lookbook/LoginActivity.java
+++ app/src/main/java/cst438/lookbook/LoginActivity.java
@@ -1,12 +1,13 @@
 package cst438.lookbook;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;  //removed this since it was giving me errors
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {//used Activity instead of AppCompTActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
