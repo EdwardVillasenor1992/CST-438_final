0785e0522dd0e023c695f05c07b1c4b409323806
esanlioglu <esanlioglu@csumb.edu>
2018-04-06 19:21:34 -0700

    Linked XML file to java class and made changes on the layout.

---
+++ app/src/main/java/cst438/lookbook/MainSearchBooks.java
@@ -0,0 +1,31 @@
+package cst438.lookbook;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainSearchBooks extends Activity implements View.OnClickListener  {
+    // private String name;
+    // private EditText titleInput;
+
+    // private String author;
+    // private EditText authorInput;
+
+    // private String isbn
+    // private EditText isbnInput
+
+    // private Button searchBookButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_search_books);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
