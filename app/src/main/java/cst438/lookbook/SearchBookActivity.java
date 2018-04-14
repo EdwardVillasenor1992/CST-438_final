7eff0eeb93202bd56dd9d8dc7e06549725aa37cf
Eduardo Villasenor <eduardovillasenor26@yahoo.com>
2018-04-13 23:41:56 -0700

    added google sign in button

--- app/src/main/java/cst438/lookbook/SearchBookActivity.java
+++ app/src/main/java/cst438/lookbook/SearchBookActivity.java
@@ -3,13 +3,29 @@ package cst438.lookbook;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SearchBookActivity extends android.support.v7.app.AppCompatActivity {
     static final String TAG = "MainActivity";
@@ -22,22 +38,70 @@ public class SearchBookActivity extends android.support.v7.app.AppCompatActivity
     private Button submitLoginButton;
     private Button mCreateAccountButton;
 
+    SignInButton button;
+    FirebaseAuth mAuth;
+    GoogleApiClient mGoogleApiClient;
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    private final static int RC_SIGN_IN = 2;
+
     private LookBookDB db;
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_book);
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        button = (SignInButton) findViewById(R.id.submit_login);
+        mAuth = FirebaseAuth.getInstance();
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null)
+                {
+                    startActivity(new Intent(SearchBookActivity.this, MainSearchBooks.class));
+
+                }
+
+            }
+        };
+
+
+        // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(SearchBookActivity.this, "something went wrong",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
-
+        ////////////////////////////////////////////////////////////////////////////////////////////
         getSupportActionBar().setDisplayOptions(android.app.ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.abs_layout);
 
@@ -47,42 +111,7 @@ public class SearchBookActivity extends android.support.v7.app.AppCompatActivity
         usernameInput = (EditText) findViewById(R.id.username_input);
         passwordInput = (EditText) findViewById(R.id.password_input);
 
-        submitLoginButton = (Button) findViewById(R.id.submit_login);
-        submitLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "Starting search book activity");
 
-                name = usernameInput.getText().toString();
-                password = passwordInput.getText().toString();
-
-                if(name.equals("") || password.equals(""))
-                {
-                    Toast.makeText(getApplicationContext(), "Please enter username and password", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-
-
-                    Toast.makeText(getApplicationContext(), "Login under development", Toast.LENGTH_SHORT).show();
-                    Toast.makeText(getApplicationContext(), "Username: " + name + " Password: " + password, Toast.LENGTH_SHORT).show();
-
-
-                    //_____________________________________________________________________________________
-                    //passing values to next activity
-                    //use:
-                    // String name = getIntent().getStringExtra("username");
-                    // String password = getIntent().getStringExtra("userPassword");
-                    Intent intent = new Intent(getBaseContext(), MainSearchBooks.class);
-                    intent.putExtra("username", name);
-                    intent.putExtra("userPassword", password);
-                    startActivity(intent);
-                    //-----------------------------------------------------------------------------------
-                }
-            }
-
-
-        });
 
         mCreateAccountButton = (Button) findViewById(R.id.create_account);
 
@@ -99,6 +128,60 @@ public class SearchBookActivity extends android.support.v7.app.AppCompatActivity
 
 
         });
+
+
+
     }
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                Toast.makeText(SearchBookActivity.this, "google sign in failed",Toast.LENGTH_LONG).show();
+
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            //Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
 
 }
