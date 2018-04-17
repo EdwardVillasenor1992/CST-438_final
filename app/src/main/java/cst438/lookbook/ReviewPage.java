ddccf6844917ef7153e772b1f2c489220fab36d1
garc2586 <31326232+garc2586@users.noreply.github.com>
2018-04-17 00:59:07 -0700

    Worked on review page, works 30% of the time.

--- app/src/main/java/cst438/lookbook/ReviewPage.java
+++ app/src/main/java/cst438/lookbook/ReviewPage.java
@@ -2,7 +2,19 @@ package cst438.lookbook;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ReviewPage extends AppCompatActivity{
 
@@ -13,10 +25,68 @@ public class ReviewPage extends AppCompatActivity{
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.review_page);
+        final TextView bookSummary = (TextView) findViewById(R.id.bookSummary);
+        final TextView bookTitle = (TextView) findViewById(R.id.reviewPageTitle);
 
-        //passedID = getIntent().getStringExtra("bookID");
+        //get book id from previous page
+        passedID = getIntent().getStringExtra("idBook");
 
-        //Toast.makeText(getApplicationContext(), "ID of passed book: " + passedID, Toast.LENGTH_SHORT).show();
+        String bookUrl = "https://www.googleapis.com/books/v1/volumes/" + passedID;
+
+        Toast.makeText(getApplicationContext(), "Book url:" + bookUrl, Toast.LENGTH_SHORT).show();
+
+        // Instantiate the RequestQueue.
+        final RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest
+        (
+                Request.Method.GET, bookUrl,
+
+                new Response.Listener<String>()
+                {
+                    JSONObject jsonObj;
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        try {
+                            jsonObj = new JSONObject(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        String description = null;
+                        String name = "Book Summary";
+                        JSONObject jArry = null;
+                        try {
+                            jArry = jsonObj.getJSONObject("volumeInfo");
+                            description = jArry.getString("description");
+                            name = jArry.getString("title");
+                        } catch (JSONException e) {
+                            description = "Could not get book summary";
+                            e.printStackTrace();
+                        }
+
+                        bookTitle.setText(name);
+                        bookSummary.setText(description);
+                        queue.stop();
+                    }
+                },
+
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        bookSummary.setText("Error getting book summary");
+                        error.printStackTrace();
+                        queue.stop();
+                    }
+                }
+        );
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
 }
